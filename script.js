e08c800ed1186fe1246668da0a8027786130c9a6
Adnan Manik <adnanmanik0@gmail.com>
2024-08-23 16:27:01 +0500

    added parallax Effect

--- script.js
+++ script.js
@@ -3,43 +3,56 @@ const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
 const layerWidth = 2400;
-const SPEED = 5;
+const SPEED = 10;
 
 
-const layer1 = new Image();
-layer1.src = 'layers/layer-1.png';
-const layer2 = new Image();
-layer2.src = 'layers/layer-2.png';
-const layer3 = new Image();
-layer3.src = 'layers/layer-3.png';
-const layer4 = new Image();
-layer4.src = 'layers/layer-4.png';
-const layer5 = new Image();
-layer5.src = 'layers/layer-5.png';
+class layers {
+    
+    constructor(image,layerName, speed ) {
+        this.x=0;
+        this.name = layerName;
+        this.image = image;
+        this.speed = SPEED * speed;
+        console.log(`speed of ${this.name} is: ${this.speed}`);
+    }
+    draw() {
+        if (this.x <= -2400) { this.x = 0; }
+
+        ctx.drawImage(this.image, this.x, 0);
+        ctx.drawImage(this.image, layerWidth + this.x, 0);
+
+        this.x -= this.speed;
+    }
+}
+
+const bgLayer1 = new Image();
+bgLayer1.src = 'layers/layer-1.png';
+const bgLayer2 = new Image();
+bgLayer2.src = 'layers/layer-2.png';
+const bgLayer3 = new Image();
+bgLayer3.src = 'layers/layer-3.png';
+const bgLayer4 = new Image();
+bgLayer4.src = 'layers/layer-4.png';
+const bgLayer5 = new Image();
+bgLayer5.src = 'layers/layer-5.png';
+
+const layer1 = new layers(bgLayer1, 'bg', 1);
+const layer2 = new layers(bgLayer2, 'farBuildings',0.4);
+const layer3 = new layers(bgLayer3, 'clouds',0.2);
+const layer4 = new layers(bgLayer4, 'nearBuildings',0.6);
+const layer5 = new layers(bgLayer5, 'ramp',1);
+
+const layerObjects = [ layer1, layer2, layer3, layer4, layer5]
 
 
-let x = SPEED;
 
-function animate(){
+function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.drawImage(layer1, x, 0);
-    ctx.drawImage(layer1, layerWidth+x, 0);
-
-    ctx.drawImage(layer2, x, 0);
-    ctx.drawImage(layer2, layerWidth+x, 0);
-
-    ctx.drawImage(layer3, x, 0);
-    ctx.drawImage(layer3, layerWidth+x, 0);
-
-    ctx.drawImage(layer4, x, 0);
-    ctx.drawImage(layer4, layerWidth+x, 0);
-
-    ctx.drawImage(layer5, x, 0);
-    ctx.drawImage(layer5, layerWidth+x, 0);
     
-    if(x<=-2400){ x=0;}
-    
-    x -= SPEED;
+    layerObjects.forEach((object)=>{
+        object.draw();
+    })
+
     requestAnimationFrame(animate);
 }
 
